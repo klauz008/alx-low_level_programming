cf55b3523c1cc6d2f31909a3f8094f5c2b004cbd
Nicholas Bassey <nickyawat@gmail.com>
2022-09-11 14:51:57 -0700

    added new program to file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point
+ * 
+ * Description: 'program to print negative numbers, positive numbers and zero'
+ * 
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
