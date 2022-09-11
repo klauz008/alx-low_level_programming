b169d9e3e06a523663f087c90510b64c2f6ae437
Nicholas Bassey <nickyawat@gmail.com>
2022-09-11 15:12:32 -0700

    added script to print random characters

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
 /**
- * main - Entry point
- * Description: 'the program's description'
- * Return: Always 0 (Success)
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
  */
-/* betty style doc for function main goes there */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
 char c;
 for (c = 'a'; c <= 'z'; c++)
 putchar(c);
